b4f7b0d7fe347e6ba0eff4a82cfa19063ac6c579
code-simplifier-mahbub <Easycodermahbubwith5500@gmail.com>
2023-06-02 17:17:56 +0600

    lint checking test with pre commit

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,17 @@
-import mongoose from "mongoose";
-import config from "./config";
-import app from "./app";
-
-
+import mongoose from 'mongoose'
+import config from './config'
+import app from './app'
 
 async function bootstrap() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log("database connection successful");
+    await mongoose.connect(config.database_url as string)
+    console.log('database connection successful')
 
     app.listen(config.port, () => {
-      console.log(`Application listening on port ${config.port}`);
-    });
+      console.log(`Application listening on port ${config.port}`)
+    })
   } catch (err) {
-    console.log("database connection error", err);
+    console.log('database connection error', err)
   }
 }
-bootstrap();
+bootstrap()
